1c40957180a0309a800fc140a7f7fcad5090ceda
apps4zap <52222876+apps4zap@users.noreply.github.com>
2019-07-07 11:52:37 -0500

    Create magic.js

---
+++ schemas/objects/magic.js
@@ -0,0 +1,44 @@
+export default {
+  type: 'object',
+  name: 'magic',
+  title: 'Magic',
+  fields: [
+     {
+       title: "Power",
+       name: "power",
+       type: "boolean"
+     },
+              {
+                 title: "Ability",
+                 name: "ability",
+                 type: "string",
+              },
+
+              {
+              	  title: "Ability Level",
+                  name: "abilitylevel",
+                  type: "number",
+               },
+               {
+               	title: "Clique",
+                   name: "clique",
+                   type: "string",
+                },
+                {
+                   title: "Trinkets",
+                   name: "trinkets",
+                   type: "string",
+                 },
+                 {
+                   title: "Musical",
+                   name: "musical",
+                   type: "boolean",
+                  },
+                 
+                   {
+                   title: "Stylo",
+                   name: "stylo",
+                   type: "string",
+                   }
+             ]
+         }
